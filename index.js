633917798c24ff15e5beb02fbc76652e7113070a
rojanjoshi <rozanjoshi@gmail.com>
2021-11-17 21:21:17 +0545

    2nd day

--- index.js
+++ index.js
@@ -6,16 +6,22 @@ const mongoose = require("mongoose")
 
 const dotenv = require("dotenv")
 
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+
+
 dotenv.config();
 
 mongoose
-.connect(
-    process.env.MONGO_URL
-    )
+.connect(process.env.MONGO_URL)
 .then(()=>console.log("DBconeection is successful"))
 .catch((err)=>{
-    console.log("error");
+    console.log(err);
 });
+
+app.use(express.json())
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.get("/api/test", ()=>{
     console.log("test is successful")
---
+++ routes/user.js
@@ -0,0 +1,22 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const {verifyToken, verifyTokenAndAuthorization} = require("./verifyToken")
+
+router.put("/:id",verifyTokenAndAuthorization,async(req,res)=>{
+  if(req.body.password){
+      req.body.password=CryptoJS.AES.encrypt(
+          req.body.password,
+          process.env.PASS_SEC
+      ).toString()
+  }
+  try {
+      const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+          $set: req.body
+      },{new:true})
+      res.status(200).json(updatedUser)
+  } catch (error) {
+      res.status(500).json(error)
+  }
+})
+
+module.exports = router
